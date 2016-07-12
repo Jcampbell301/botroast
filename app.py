539502cd39deb44a82a1638eacb23ce1905657a3
Subhodh Kotekal <subhodh.kotekal@gmail.com>
2016-07-12 15:43:54 -0500

    Bot posts json data now

--- app.py
+++ app.py
@@ -2,14 +2,21 @@ import flask
 import json
 from flask import request
 import bot
+import requests
 
 app = flask.Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	if request.method == 'POST':
-		bot.post(request.get_json())
+	# if request.method == 'POST':
+		# bot.post(request.get_json())
+		# bot.post('Lorem ipsum')
 	return flask.render_template('index.html')
-	
+
+@app.route('/post', methods=['POST'])
+def groupme_post():
+	if request.method == 'POST':
+		bot.post(request.get_data())
+	return flask.render_template('index.html')
 if __name__ == '__main__':
 	app.run(host='0.0.0.0')
--- bot.py
+++ bot.py
@@ -6,5 +6,6 @@ BOT_ID = '6c059ca743bdf06151f0e4ef6c'
 URL = 'https://api.groupme.com/v3/'
 POST_URL ='https://api.groupme.com/v3/bots/post/'
 
-def post(text):
-	r = requests.post(POST_URL, data={"bot_id": BOT_ID, "text": text})
+def post(msg):
+	r = requests.post(POST_URL, data={"bot_id": BOT_ID, "text": msg})
+	return r.text
